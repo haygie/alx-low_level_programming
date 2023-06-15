c3c82feb16755b07da4448f41ddbfd9cc52d267a
haygie <diego.28jantjies@gmail.com>
2023-06-15 12:30:27 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
  * main - Entry point
  *
- * Return:Always 0 (Success)
+ * Return: 0 if Success
  */
 int main(void)
 {
 	printf("with proper grammer, but the outcome is a piece of art,\n");
-	return (0)
+	return (0);
 }
