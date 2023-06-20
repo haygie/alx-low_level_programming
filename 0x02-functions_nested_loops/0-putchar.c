d0fd3acd6a9f0f08e05490fd153708715295c274
haygie <diego.28jantjies@gmail.com>
2023-06-20 14:09:07 -0700

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,19 +2,20 @@
 
 /**
  * main - Entry point
- * Description: Prints _putchar
- * Return: Always 0 (Success)
+ *
+ * Return : Always 0
  */
+
 int main(void)
 {
-_putchar ('_');
-_putchar ('p');
-_putchar ('u');
-_putchar ('t');
-_putchar ('c');
-_putchar ('h');
-_putchar ('a');
-_putchar ('r');
-_putchar ('\n');
-return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_HEADER_FILES
-#define MAIN_HEADER_FILES
+#ifndef MAIN_HEADER_FILE
+#define MAIN_HEADER_FILE
 
 int _putchar(char c);
 void print_alphabet(void);
@@ -13,7 +13,5 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-void print_times_table(int n);
 
 #endif
-
