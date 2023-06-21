d2910d2c0b8367acdcbcd259cd0601ade6250721
haygie <diego.28jantjies@gmail.com>
2023-06-21 04:44:29 -0700

    done

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -5,10 +5,12 @@
 * fibonacci suit numbers
 * Return: return 0
 */
-	int main(void)
+int main(void)
 {
 	int inc;
+
 	unsigned long n1 = 0, n2 = 1, n3;
+
 	for (inc = 0; inc < 50; inc++)
 	{
 	n3 = n1 + n2;
